6ca1241b2d84ad9c4b4eea3eb493b29647bf761c
marvelph <marvel@programmershigh.org>
2024-11-29 23:28:33 +0900

    Add movement command

---
+++ Plugins/movement.lua
@@ -0,0 +1,40 @@
+plugin {
+    id = "org.programmershigh.movement",
+    name = "Movement",
+    apiVersion = "2.2",
+    author = "Kenji Nishishiro",
+    email = "marvel@programmershigh.org",
+    url = "https://github.com/marvelph/MartaPlugins"
+}
+
+action {
+    id = "move.next.selection",
+    name = "Move to Next Selection",
+    apply = function(context)
+        local model = context.activePane.model
+        if model.hasCurrent then
+            for i = model.currentIndex + 1, model.lastIndex do
+                if model:isSelected(i) then
+                    model.currentIndex = i
+                    break
+                end
+            end
+        end
+    end
+}
+
+action {
+    id = "move.previous.selection",
+    name = "Move to Previous Selection",
+    apply = function(context)
+        local model = context.activePane.model
+        if model.hasCurrent then
+            for i = model.currentIndex - 1, 0, -1 do
+                if model:isSelected(i) then
+                    model.currentIndex = i
+                    break
+                end
+            end
+        end
+    end
+}
