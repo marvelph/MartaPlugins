3f576e4069a5809c94ab77d856536b1438c7d7d4
marvelph <marvel@programmershigh.org>
2024-11-30 10:47:02 +0900

    Fix selection command

--- Plugins/selection.lua
+++ Plugins/selection.lua
@@ -7,49 +7,31 @@ plugin {
     url = "https://github.com/marvelph/MartaPlugins"
 }
 
-function find(inactiveModel, info)
-    for i = 0, inactiveModel.lastIndex do
-        local inactiveItem = inactiveModel:getItem(i)
-        local inactiveInfo = inactiveItem.info
-        if inactiveItem.kind == "file" and inactiveInfo and inactiveInfo.isFile and inactiveInfo.name == info.name then
-            return true
+function find(inactiveModel, item)
+    local info = item.info
+    if item.kind == "file" and info and info.isFile then
+        for i = 0, inactiveModel.lastIndex do
+            local inactiveItem = inactiveModel:getItem(i)
+            local inactiveInfo = inactiveItem.info
+            if inactiveItem.kind == "file" and inactiveInfo and inactiveInfo.isFile and inactiveInfo.name == info.name then
+                return true
+            end
         end
     end
     return false
 end
 
 action {
-    id = "select.extension",
-    name = "Select Extension",
-    apply = function(context)
-        local model = context.activePane.model
-        if model.hasCurrent then
-            local currentItem = model:getItem(model.currentIndex)
-            local currentInfo = currentItem.info
-            if currentItem.kind == "file" and currentInfo and currentInfo.isFile then
-                for i = 0, model.lastIndex do
-                    local item = model:getItem(i)
-                    local info = item.info
-                    if item.kind == "file" and info and info.isFile and info.extension == currentInfo.extension then
-                        model:select(i)
-                    end
-                end
-            end
-        end
-    end
-}
-
-action {
     id = "select.same.file",
     name = "Select Same File",
     apply = function(context)
-        local model = context.activePane.model
-        for i = 0, model.lastIndex do
-            local item = model:getItem(i)
-            local info = item.info
-            local inactivePane = context.inactivePane
-            if item.kind == "file" and info and inactivePane and info.isFile and find(inactivePane.model, info) then
-                model:select(i)
+        local inactivePane = context.inactivePane
+        if inactivePane then
+            local model = context.activePane.model
+            for i = 0, model.lastIndex do
+                if find(inactivePane.model, model:getItem(i)) then
+                    model:select(i)
+                end
             end
         end
     end
@@ -59,13 +41,13 @@ action {
     id = "select.different.file",
     name = "Select Different File",
     apply = function(context)
-        local model = context.activePane.model
-        for i = 0, model.lastIndex do
-            local item = model:getItem(i)
-            local info = item.info
-            local inactivePane = context.inactivePane
-            if item.kind == "file" and info and inactivePane and info.isFile and not find(inactivePane.model, info) then
-                model:select(i)
+        local inactivePane = context.inactivePane
+        if inactivePane then
+            local model = context.activePane.model
+            for i = 0, model.lastIndex do
+                if not find(inactivePane.model, model:getItem(i)) then
+                    model:select(i)
+                end
             end
         end
     end
@@ -96,6 +78,27 @@ action {
             local info = item.info
             if item.kind == "file" and info and info.isFile then
                 model:invertSelection(i)
+            end
+        end
+    end
+}
+
+action {
+    id = "select.extension",
+    name = "Select Extension",
+    apply = function(context)
+        local model = context.activePane.model
+        if model.hasCurrent then
+            local currentItem = model:getItem(model.currentIndex)
+            local currentInfo = currentItem.info
+            if currentItem.kind == "file" and currentInfo and currentInfo.isFile then
+                for i = 0, model.lastIndex do
+                    local item = model:getItem(i)
+                    local info = item.info
+                    if item.kind == "file" and info and info.isFile and info.extension == currentInfo.extension then
+                        model:select(i)
+                    end
+                end
             end
         end
     end
