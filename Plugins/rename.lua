d0c2b36819ad8daf8a10e49dda7fb23e1115dcdd
marvelph <marvel@programmershigh.org>
2026-03-20 19:11:23 +0900

    Fix rename command

--- Plugins/rename.lua
+++ Plugins/rename.lua
@@ -8,13 +8,35 @@ plugin {
 }
 
 local function prompt()
-    local script = 'display dialog "Pattern" with title "Rename with Pattern" default answer "{index:2}-{name}.{extension}" buttons {"Cancel", "Rename"} default button "Rename"'
+    local script = 'display dialog "Pattern" with title "Rename with Pattern" default answer "{index:2}-{name}.{extension}" buttons {"Cancel", "Rename"} default button "Rename" cancel button "Cancel"'
     local handle = io.popen("osascript -e '" .. script .. "'")
     local result = handle:read("*a")
     handle:close()
     local button, text = result:match("button returned:(.*), text returned:([^\n]*)")
     if button == "Rename" then
         return text
+    else
+        return nil
+    end
+end
+
+local function alert(message, description)
+    message = message:gsub('"', '" & quote & "')
+    message = message:gsub('\n', '" & return & "')
+    description = description:gsub('"', '" & quote & "')
+    description = description:gsub('\n', '" & return & "')
+    local script = 'display alert "' .. message .. '" message "' .. description .. '" as warning buttons {"Skip", "Abort"} default button "Abort" cancel button "Skip"'
+    local path = os.tmpname()
+    local file = io.open(path, "w")
+    file:write(script)
+    file:close()
+    print(script)
+    local handle = io.popen("osascript '" .. path .. "'")
+    local result = handle:read("*a")
+    handle:close()
+    local button, text = result:match("button returned:([^\n]*)")
+    if button == "Abort" then
+        return true
     else
         return nil
     end
@@ -54,7 +76,19 @@ action {
             if info.isFile then
                 local file = info.file
                 local name = buildName(pattern, index, file.nameWithoutExtension, file.extension)
-                file:rename(file.parent:resolve(name).path)
+                local error = file:rename(file.parent:resolve(name).path)
+                if error then
+                    print(error.description)
+                    local message = "Can't rename \"" .. file.nameWithoutExtension .. "\""
+                    local description = error.description .. "\n\n"
+                    description = description .. "Full path: " .. tostring(file.path)
+                    local abort = alert(message, description)
+                    print(abort)
+                    if abort then
+                        return
+                    end
+                end
+
                 index = index + 1
             end
         end
